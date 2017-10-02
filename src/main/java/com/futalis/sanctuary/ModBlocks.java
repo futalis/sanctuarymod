1e5fe984c2bddd2bb37bd650364d64f0aefa8ac3
futalis <virusleymaker@yahoo.com>
2017-10-01 20:52:21 -0700

    Added texture to SanctuaryBlock

--- src/main/java/com/futalis/sanctuary/ModBlocks.java
+++ src/main/java/com/futalis/sanctuary/ModBlocks.java
@@ -1,6 +1,9 @@
 package com.futalis.sanctuary;
 
 import com.futalis.sanctuary.blocks.BlockSanctuaryBlock;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -12,6 +15,8 @@ public class ModBlocks {
 
     @SideOnly(Side.CLIENT)
     public static void initModels() {
+        ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(blockSanctuaryBlock), 0,
+                new ModelResourceLocation(blockSanctuaryBlock.getRegistryName(), "inventory"));
     }
 
     @SideOnly(Side.CLIENT)
